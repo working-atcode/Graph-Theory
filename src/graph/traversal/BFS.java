1ea418b888e25d276d90a3adf7b10796244b0551
Alka Raghuvanshi <AlkaRaghuvanshi@fico.com>
2020-03-22 02:19:04 +0530

    Refactor code

--- src/graph/traversal/BFS.java
+++ src/graph/traversal/BFS.java
@@ -7,36 +7,10 @@ import java.util.LinkedList;
 
 public class BFS {
 
-    public void bfsTraversal(AdjacencyListGraph graph, int start){
-
-        //mark visited node
-        boolean visited[] = new boolean[graph.V];
-
-        //queue for bfs
-        LinkedList<Integer> queue = new LinkedList <>();
-
-        visited[start] = true;
-        queue.add(start);
-
-        while(queue.size()!=0){
-            start = queue.poll();
-            System.out.print(start+ " ");
-
-            Iterator <Integer> i = graph.adjListArray[start].listIterator();
-            while(i.hasNext()){
-                int n = i.next();
-                if(!visited[n]){
-                    visited[n]=true;
-                    queue.add(n);
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) {
 
         AdjacencyListGraph graph = new AdjacencyListGraph(4);
-        graph.addEdge( 0,1);
+        graph.addEdge(0, 1);
         graph.addEdge(0, 2);
         graph.addEdge(1, 2);
         graph.addEdge(2, 0);
@@ -45,5 +19,31 @@ public class BFS {
         BFS bfs = new BFS();
         bfs.bfsTraversal(graph, 2);
 
+    }
+
+    public void bfsTraversal(AdjacencyListGraph graph, int start) {
+
+        //mark visited node
+        boolean visited[] = new boolean[graph.V];
+
+        //queue for bfs
+        LinkedList <Integer> queue = new LinkedList <>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (queue.size() != 0) {
+            start = queue.poll();
+            System.out.print(start + " ");
+
+            Iterator <Integer> i = graph.adjListArray[start].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
     }
 }

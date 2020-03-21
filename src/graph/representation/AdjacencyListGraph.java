0fb28910713acb76470dfbe5c0178a193cd3abf0
Alka Raghuvanshi <AlkaRaghuvanshi@fico.com>
2020-03-22 01:23:57 +0530

    Add utility function to print adjacency list

--- src/graph/representation/AdjacencyListGraph.java
+++ src/graph/representation/AdjacencyListGraph.java
@@ -1,33 +1,42 @@
 package graph.representation;
 
-import com.sun.corba.se.impl.orbutil.graph.Graph;
+import graph.utility.GraphUtility;
 
 import java.util.LinkedList;
 
-public class AdjacencyList {
+public class AdjacencyListGraph {
 
-    int V;
-    LinkedList<Integer> adjListArray[];
+    public int V;
+    public LinkedList <Integer> adjListArray[];
 
-    AdjacencyList(int V){
+    AdjacencyListGraph(int V) {
         this.V = V;
 
         adjListArray = new LinkedList[V];
 
-        for(int i=0;i<V;i++){
+        for (int i = 0; i < V; i++) {
             adjListArray[i] = new LinkedList <>();
         }
 
     }
 
-    static void addEdge(AdjacencyList adjacencyList, int src, int dest){
-        //assumes undirected graph
+    static void addEdge(AdjacencyListGraph adjacencyList, int src, int dest) {
         adjacencyList.adjListArray[src].add(dest);
-        adjacencyList.adjListArray[dest].add(src);
+
+        //assumes undirected graph, so add an edge to represent both direction
+        adjacencyList.adjListArray[dest].add(src); // remove this if this is an directed graph
     }
 
     public static void main(String[] args) {
         int V = 5;
-        
+        AdjacencyListGraph graph = new AdjacencyListGraph(V);
+        addEdge(graph, 0, 1);
+        addEdge(graph, 0, 4);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 1, 3);
+        addEdge(graph, 1, 4);
+        addEdge(graph, 2, 3);
+        addEdge(graph, 3, 4);
+        GraphUtility.printAdjacencyListGraph(graph);
     }
 }
--- src/graph/utility/GraphUtility.java
+++ src/graph/utility/GraphUtility.java
@@ -1,4 +1,17 @@
 package graph.utility;
 
+import graph.representation.AdjacencyListGraph;
+
 public class GraphUtility {
+
+    public static void printAdjacencyListGraph(AdjacencyListGraph graph) {
+
+        for (int v = 0; v < graph.V; v++) {
+            System.out.print(v);
+            for (int items : graph.adjListArray[v]) {
+                System.out.print(" -> " + items);
+            }
+            System.out.println();
+        }
+    }
 }
